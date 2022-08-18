09ca70199b10f0a8e8e887c0b81d9eab688bd5eb
sarard2 <99599777+sarard2@users.noreply.github.com>
2022-08-18 21:02:01 +0300

    Update pred.py

--- pred.py
+++ pred.py
@@ -33,30 +33,6 @@ st.markdown("""
 </style>
 """,unsafe_allow_html=True)
 
-st.markdown("""
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="home.jpg" class="d-block w-100" alt="First">
-    </div>
-    <div class="carousel-item">
-      <img src="home.jpg" class="d-block w-100" alt="Middle">
-    </div>
-    <div class="carousel-item">
-      <img src="home.jpg" class="d-block w-100" alt="Last">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-""",unsafe_allow_html=True)
-
 #st.markdown("""
 #<div class="alert alert-secondary" role="alert">
 #</div>
