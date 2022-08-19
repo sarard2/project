9e0ec1b88aadad21a708d200b3ca769c432d5501
sarard2 <99599777+sarard2@users.noreply.github.com>
2022-08-19 08:59:07 +0300

    Update pred.py

--- pred.py
+++ pred.py
@@ -273,8 +273,5 @@ if selected=="Prediction":
         fig1.update_yaxes(showgrid=False,showticklabels = True)
         st.plotly_chart(fig1)
 
-    st.markdown("""
-    <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
-      Popover on top 
-     </button>""",unsafe_allow_html=True)
+    st.markdown("""<button type="button" class="btn btn-lg btn-danger" data-bs-toggle="popover" data-bs-title="Popover title" data-bs-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>""",unsafe_allow_html=True)
   
