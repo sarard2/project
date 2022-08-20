3ce0a9584cb0483b33507e8fe1cd7ebab855d755
sarard2 <99599777+sarard2@users.noreply.github.com>
2022-08-20 19:33:35 +0300

    Update pred.py

--- pred.py
+++ pred.py
@@ -291,7 +291,10 @@ if selected=="Prediction":
         fig2.update_yaxes(showgrid=False,showticklabels = True)
         st.plotly_chart(fig2)
     with col2:
-        st.dataframe(forecast[["ds","yhat"]])
+     values=forecast[["ds","yhat"]]
+     values.columns("Date","Expected Revenue")
+     AgGrid(values)
+    
     col1,col2=st.columns([2,1])
     with col1:
      fig1=plot_components_plotly(model, forecast)
