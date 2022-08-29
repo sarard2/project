a863ea6320f37f6206d8d68f92f9e0bc54e56dd9
sarard2 <99599777+sarard2@users.noreply.github.com>
2022-08-29 19:10:06 +0300

    Update pred.py

--- pred.py
+++ pred.py
@@ -280,8 +280,8 @@ if selected=="Transactions":
     """,unsafe_allow_html=True)
     
     number = st.number_input('Top N Rules',min_value=2,max_value=50)
-    support = st.slider('Support',0,1,1,step=float)
-    confidence = st.slider('Confidence',0,1,1)
+    support = st.slider('Support',0.0005,1.0,0.004)
+    confidence = st.slider('Confidence',0.002,1.0,0.05)
     AgGrid(rules)
      
     unique=sales.groupby('InvoiceID')["Quantity"].count().reset_index()
